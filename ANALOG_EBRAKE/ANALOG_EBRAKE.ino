a788349f9ca5c7902f849729fb48ada53797a3c8
AM-STUDIO <amstudiodesign7@gmail.com>
2017-02-01 12:39:54 +1100

    Add files via upload

---
+++ ANALOG_EBRAKE/ANALOG_EBRAKE.ino
@@ -0,0 +1,21 @@
+// Analog eBrake-Handbrake
+// PIN A0 to 10k pot output - Throttle Axis
+// Use with Arduino ProMicro.
+// Tested and working in DiRT RALLY + ASSETTO CORSA
+// by AMSTUDIO
+// 20.1.2017
+
+#include <Joystick.h>
+
+void setup()
+
+{pinMode(A0, INPUT); 
+   Joystick.begin();}
+
+const int pinToButtonMap = A0;
+
+void loop()
+
+{int pot = analogRead(A0);
+int mapped = map(pot,0,1023,0,255);
+{Joystick.setThrottle(mapped);}}
